1b6d9f3682a424dcdfdd286a8cb816e5ad017777
aya <https://github.com/ayamashalyy>
2024-11-13 15:26:25 +0200

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by aya on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
